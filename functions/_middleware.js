782adfe0fe62e7b933724e7c5d827f00a0ce67bf
Marius Gundersen <marius.gundersen@clave.no>
2024-12-08 22:15:40 +0100

    fixed

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
 export async function onRequest({ request, env, next }) {
   const url = new URL(request.url);
   if (request.headers.get('authorization') === `bearer ${env.SECRET}`) return await next();
-  if (request.headers.get('cookie').includes(`login=${env.SECRET}`)) return await next();
+  if (request.headers.get('cookie')?.includes(`login=${env.SECRET}`) ?? false) return await next();
 
   if (url.pathname === '/login') return await next();
   return Response.redirect(new URL('/login', url));
