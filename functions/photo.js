dca4d7d62bfa91be4ae71ca8d456264ee436a8d9
Marius Gundersen <marius.gundersen@clave.no>
2024-12-10 11:19:45 +0100

    Handle If-None-Match

--- functions/photo.js
+++ functions/photo.js
@@ -1,9 +1,15 @@
+
+
 export async function onRequest({ request, env }) {
   const listed = await env.BUCKET.list({ prefix: 'img-', limit: 1 });
-  const object = await env.BUCKET.get(listed.objects[0].key);
+  const object = await env.BUCKET.get(listed.objects[0].key, { onlyIf: request.headers });
 
-  if (object === null) {
+  if (!object) {
     return new Response("Object Not Found", { status: 404 });
+  }
+
+  if (!object.body) {
+    return new Response(null, { status: 304 });
   }
 
   const headers = new Headers();
