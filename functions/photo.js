21436b9d1dea7b71da0c43e75d596b4f5b61bae8
Marius Gundersen <me@mariusgundersen.net>
2024-12-15 18:46:03 +0100

    Log requests

--- functions/photo.js
+++ functions/photo.js
@@ -4,6 +4,12 @@ export async function onRequest({ request, env }) {
   const listed = await env.BUCKET.list({ prefix: 'img-', limit: 1 });
   const object = await env.BUCKET.get(listed.objects[0].key, { onlyIf: request.headers });
 
+  await env.KV.put(Date.now(), JSON.stringify({
+    ip: request.headers.get('CF-Connecting-IP'),
+    status: object ? object.body ? 200 : 304 : 404,
+    key: object?.key
+  }));
+  
   if (!object) {
     return new Response("Object Not Found", { status: 404 });
   }
