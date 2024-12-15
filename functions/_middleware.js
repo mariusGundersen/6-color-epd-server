691c23d7fdcd4969260828ea8e2e553462dcd7a9
Marius Gundersen <me@mariusgundersen.net>
2024-12-15 17:02:45 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ export async function onRequest({ request, env, next }) {
   const url = new URL(request.url);
   if (request.headers.get('authorization') === `bearer ${env.SECRET}`) return await next();
   if (request.headers.get('cookie')?.includes(`login=${env.SECRET}`) ?? false) return await next();
-
-  if (url.pathname === '/login') return await next();
+ 
+  if (['/login', '/manifest.json'].includes(url.pathname)) return await next();
   return Response.redirect(new URL('/login', url));
 }
