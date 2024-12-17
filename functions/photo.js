defd3e1b520dd150c018af4ae5e2ffae3a10f3b4
Marius Gundersen <marius.gundersen@clave.no>
2024-12-17 13:24:50 +0100

    key should be readable

--- functions/photo.js
+++ functions/photo.js
@@ -4,7 +4,7 @@ export async function onRequest({ request, env }) {
   const listed = await env.BUCKET.list({ prefix: 'img-', limit: 1 });
   const object = await env.BUCKET.get(listed.objects[0].key, { onlyIf: request.headers });
 
-  await env.KV.put(Date.now(), JSON.stringify({
+  await env.KV.put(new Date().toISOString(), JSON.stringify({
     ip: request.headers.get('CF-Connecting-IP'),
     status: object ? object.body ? 200 : 304 : 404,
     key: object?.key,
