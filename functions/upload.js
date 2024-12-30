8b7a927cc989e01590f8adecc18a9653137875ca
Marius Gundersen <me@mariusgundersen.net>
2024-12-30 23:07:34 +0100

    Store r2 reference in KV

--- functions/upload.js
+++ functions/upload.js
@@ -1,5 +1,10 @@
 export async function onRequest({ request, env }) {
   const key = `img-${Math.floor(2 ** 32 - Date.now() / 1000)}.jpeg`;
   await env.BUCKET.put(key, request.body);
+
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const [date, time] = tomorrow.toISOString().split('T');
+  await env.KV.put(`img-${date}`, key);
   return new Response(`Put ${key} successfully!`);
 }
