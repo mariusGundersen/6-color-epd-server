ea38248b012193752c7df10bc3eaca6070b5a812
Marius Gundersen <me@mariusgundersen.net>
2025-01-05 08:14:04 +0100

    easier testing

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,13 @@
 export async function onRequest({ request, env, next }) {
   const url = new URL(request.url);
-  if (request.headers.get('authorization') === `bearer ${env.SECRET}`) return await next();
-  if (request.headers.get('cookie')?.includes(`login=${env.SECRET}`) ?? false) return await next();
- 
-  if (['/login', '/manifest.json'].includes(url.pathname)) return await next();
-  return Response.redirect(new URL('/login', url));
+  if (!env.SECRET) return await next();
+
+  if (request.headers.get("authorization") === `bearer ${env.SECRET}`)
+    return await next();
+
+  if (request.headers.get("cookie")?.includes(`login=${env.SECRET}`) ?? false)
+    return await next();
+
+  if (["/login", "/manifest.json"].includes(url.pathname)) return await next();
+  return Response.redirect(new URL("/login", url));
 }
